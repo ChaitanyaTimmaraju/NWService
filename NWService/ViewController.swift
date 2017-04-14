507bfa0880b27da03d3100fc54aeac12a97589e8
krsnachaitanya59@gmail.com <krsnachaitanya59@gmail.com>
2017-04-14 17:32:44 -0500

    Initial Commit

---
+++ NWService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NWService
+//
+//  Created by TIMMARAJU SAI V on 4/14/17.
+//  Copyright © 2017 OSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
